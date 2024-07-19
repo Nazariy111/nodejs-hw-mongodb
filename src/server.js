ebf68c7bd2918c95caeebe79a30e695691776ab9
Nazariy111 <gerikgiga1@gmail.com>
2024-07-19 11:33:49 +0300

    new try

--- src/server.js
+++ src/server.js
@@ -15,11 +15,10 @@ const PORT = Number(env(ENV_VARS.PORT, '3000'));
 export const setupServer = () => {
     const app = express();
 
-    app.use(express.json());
-    app.use(cookieParser());
-
-    app.use(logger);
+    // app.use(logger);
     app.use(cors());
+    app.use(cookieParser());
+    app.use(express.json());
 
     app.use('/api-docs', swaggerDocs());
 
